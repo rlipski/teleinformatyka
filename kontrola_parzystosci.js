2a3a5bf03a32fae90974fa366385ee3c95965310
Nokimo761 <lipskipiotr42@gmail.com>
2020-05-29 20:49:19 +0200

    parity code 2

--- kontrola_parzystosci.js
+++ kontrola_parzystosci.js
@@ -1,14 +1,12 @@
 function kontrolaParzystosci(dane){
-    $("#btnZakloc").click(function(){
-        distorion();
-       });
+
     var liczba_bitow = dane.length;
     var stringZakodowany="";
     var liczba_bajtow = liczba_bitow/8;
     liczba_bitow += 1;
 
     zakodowane = new Array(liczba_bitow);
-    typ = new Array(liczba_bitow);
+    type = new Array(liczba_bitow);
     initTypes();
 
     var ileJedynek;
@@ -31,7 +29,84 @@ function kontrolaParzystosci(dane){
     $('#daneZakodowane1').text(stringZakodowany)
 
 }
+
+
+function dekodujKontrolaParzystosci(coded_data) {
+    var n = coded_data.length;
+    var bytes = n/9;
+    dane = new Array(bytes*8);
+    var ileJedynek;
+    errors=0;
+    for (var i=0; i<bytes; i++) {
+        ileJedynek=0;
+        for (var j=0; j<8; j++) {
+            dane[i*8+j] = coded_data[i*9+j+1];
+            ileJedynek+=coded_data[i*9+j+1];
+        }
+        ileJedynek+=coded_data[i*9];	// bit parzystości
+        if (ileJedynek%2==0) {	// nie wykryto błędów
+            type[i*9]=3;				// zaznacz poprawny bit kontrolny
+            for (var j=1; j<9; j++) type[i*9+j]=0;	// zaznacz poprawne bity danych
+        }
+        else	// wystąpiły błędy
+        {
+            errors++;
+            type[i*9]=5;    // zaznacz niepewny bit kontrolny
+            for (var j=1; j<9; j++) type[i*9+j]=2;	// zaznacz niepewne bity danych
+        }
+    }
+    console.log(dane);
+    $('#daneWyjściowe').text(dane.join(''));
+}
+
+function fixKontrolaParzystosci(decoded_data) {
+    var n = decoded_data.length;
+    initTypes(); // zainicjuj tablicę rodzajów bitów
+    var bytes = n/9;
+    errors = 0;
+    var ileJedynek;
+    
+    for (var i = 0; i < bytes; i++) {
+        ileJedynek = 0;
+        
+        for(var j = 0; j < 8; j++) {
+            ileJedynek += decoded_data[i*9+j+1];  // policz "1"
+        }
+        ileJedynek+=decoded_data[i*9];	// bit parzystości
+        if (ileJedynek%2===0)	// nie wykryto błędów
+        {
+                type[i*9]=3;				// zaznacz poprawny bit kontrolny
+                for (var j=1; j<9; j++) type[i*9+j]=0;	// zaznacz poprawne bity danych
+        }
+        else	// wystąpiły błędy
+        {
+                errors++;
+                type[i*9]=5;				// zaznacz niepewny bit kontrolny
+                for (var j=1; j<9; j++) type[i*9+j]=2;	// zaznacz niepewne bity danych
+        }
+    }
+    let html=''
+    for(let i =0;i<decoded_data.length;i++){
+        if(type[i]===0){
+            html+='<span class="bg-success">'+decoded_data[i]+'</span>';
+        }else if(type[i]===1){
+            html+='<span class="bg-danger">'+decoded_data[i]+'</span>';
+        }else if(type[i]===2){
+            html+='<span class="bg-warning">'+decoded_data[i]+'</span>';
+        }
+        else if(type[i]===3){
+            html+='<span class="bg-primary">'+decoded_data[i]+'</span>';
+        }else if(type[i]===4){
+            html+='<span class="bg-dark">'+decoded_data[i]+'</span>';
+        }else if(type[i]===5){
+            html+='<span class="bg-secondary">'+decoded_data[i]+'</span>';
+        }
+
+    }
+    $('#danePoKorekcji').html(html);
+    $('#przeslaneBity').text($('#inputDane').val().length)
+}
 function initTypes() {
-    for(var i = 0; i < typ.length; i++)
-        typ[i] = 0;
+    for(var i = 0; i < type.length; i++)
+        type[i] = 0;
 }
