a86e19627f5bb9dd20088caa94795f268fdb9d17
Radosław Lipski <rlipski76@gmail.com>
2020-05-29 23:41:08 +0200

    CRC 12, 16, 32, SDLC

--- crc.js
+++ crc.js
@@ -1,3 +1,128 @@
-function crc(){
-    alert('dziala crc')
+class Crc {
+    constructor(algorithm) {
+        switch (algorithm) {
+            case '12':
+                this.klucz = 0x18005;
+                this.keyLength = 12;
+            break;
+            case '16':
+                this.klucz = 0x18005;
+                this.keyLength = 16;
+            break;
+            case '32':
+                this.klucz = 0x104C11DB7;
+                this.keyLength = 32;
+            break;
+            case 'SDLC':
+                this.klucz = 0x18005;
+                this.keyLength = 16;
+            break;
+        }
+    }
+
+    // XOR
+    xor(a, b) {
+        if (a == b)
+            return 0;
+        else
+            return 1;
+    }
+
+    // funkcja pomocnicza - inicjowanie tablicy zerami
+    initTable(table) {
+        for (var i = 0; i < table.length; i++)
+            table[i] = 0;
+        return table;
+    }
+
+    /*
+     * kopiowanie fragmentu tablicy do innej tablicy
+     * parametry:
+     * src_table: tablica, z której kopiujemy
+     * src_start: pierwszy kopiowany element
+     * dest_table: tablica, do której kopiujemy
+     * dest_start: indeks 1-wszego skopiowanego elementu
+     * length: liczba kopiowanych elementów
+     */
+    arrayCopy(src_table, src_start, dest_table, dest_start, length) {
+        for (var i = 0; i < length; i++) {
+            dest_table[dest_start + i] = src_table[src_start + i];
+        }
+
+        return dest_table;
+    }
+
+    /* policz kod CRC */
+    countCRC(bits) {
+        var n = bits.length;
+        var debug = '';
+        var crc = new Array(this.keyLength);
+        var temp = new Array(n + this.keyLength);
+        this.initTable(temp);
+        this.initTable(crc);
+        temp = this.arrayCopy(bits, 0, temp, this.keyLength, n);
+        var tklucz = new Array(this.keyLength + 1);
+        for (var i = 0; i < this.keyLength + 1; i++) {
+            debug += this.klucz & (1 << i) + '\n';
+            if ((this.klucz & (1 << i)) == 0) tklucz[i] = 0; // ok
+            else tklucz[i] = 1;
+        }
+
+        // liczenie CRC
+        for (var start = n + this.keyLength - 1; start > this.keyLength - 1; start--) {
+            if (temp[start] == 1) {
+                for (var i = 0; i < this.keyLength + 1; i++) {
+                    temp[start - i] = this.xor(temp[start - i], tklucz[this.keyLength - i]);
+                }
+            }
+        }
+
+        crc = this.arrayCopy(temp, 0, crc, 0, this.keyLength);
+
+        return crc;
+    }
+
+    /* kodowanie */
+    crc(random_data) {
+        var n = random_data.length;
+        var l = n + this.keyLength;
+        var coded_data = new Array(l);
+        var type = new Array(l);
+        coded_data = this.initTable(coded_data);
+        coded_data = this.arrayCopy(random_data, 0, coded_data, this.keyLength, n);
+        var crc = this.countCRC(random_data);
+        this.arrayCopy(crc, 0, coded_data, 0, this.keyLength);
+        for (var i = 0; i < this.keyLength; i++) type[i] = 3;
+        for (var i = this.keyLength; i < l; i++) type[i] = 0;
+        viewCode(coded_data);
+        return coded_data;
+    }
+
+    /* dekodowanie */
+    decodeCRC(coded_data) {
+        var l = coded_data.length;
+        var n = l - this.keyLength;
+        let dane = new Array(n);
+        for (var i = 0; i < n; i++) dane[i] = coded_data[i + this.keyLength];
+    }
+
+    /* korekcja */
+    fixCRC(coded_data) {
+        let errors = 0;
+        var l = coded_data.length;
+        let type = new Array(l);
+        var crc = this.countCRC(coded_data);
+        var ok = true;
+        for (var i = 0; i < this.keyLength && ok; i++) if (crc[i] != 0) ok = false;
+        if (ok) {
+            for (var i = 0; i < this.keyLength; i++) type[i] = 3;
+            for (var i = this.keyLength; i < l; i++) type[i] = 0;
+        }
+        else {
+            errors++;
+            for (var i = 0; i < this.keyLength; i++) type[i] = 5;
+            for (var i = this.keyLength; i < l; i++) type[i] = 2;
+        }
+    }
+
 }
--- method.js
+++ method.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+
    $("#btnGeneruj").click(function(){
     generateRadnomCode();
    });
@@ -21,23 +21,36 @@ $(document).ready(function(){
             case 'hamming':
                 kodowanieHamminga(dane)
               break;
-            case 'crc':
-                crc(dane)
+            case 'crc12':
+                var crc_12 = new Crc('12');
+                crc_12.crc(dane)
+              break;
+            case 'crc16':
+                var crc_16 = new Crc('16');
+                crc_16.crc(dane)
+              break;
+            case 'crc32':
+                var crc_32 = new Crc('32');
+                crc_32.crc(dane)
+              break;
+            case 'crcSDLC':
+                var crc_sdlc = new Crc('SDLC');
+                crc_sdlc.crc(dane)
               break;
             default:
               // code block
           }
-       } 
+       }
    });
 
    $('#btnDekoduj').click(function(){
 
     let dane = $('#inputDane').val();
     let daneZakodowane = $('#daneZakodowane').text();
-  
+
         var metoda;
        if($('#daneZakodowane').text()!=''){
-        
+
             daneZakodowane=stringToArray(daneZakodowane);
             dane= stringToArray(dane);
             $('.asd a').each(function(){
@@ -51,9 +64,21 @@ $(document).ready(function(){
                 case 'hamming':
                     fixKodowanieHamminga(daneZakodowane)
                 break;
-                case 'crc':
-                    fixCrc(daneZakodowane)
-                break;
+                case 'crc12':
+                    var crc_12 = new Crc('12');
+                    crc_12.fixCRC(daneZakodowane)
+                    break;
+                case 'crc16':
+                    var crc_16 = new Crc('16');
+                    crc_16.fixCRC(daneZakodowane)
+                    break;
+                case 'crc32':
+                    var crc_32 = new Crc('32');
+                    crc_32.fixCRC(daneZakodowane)
+                    break;
+                case 'crcSDLC':
+                    var crc_sdlc = new Crc('SDLC');
+                    crc_sdlc.fixCRC(daneZakodowane)
                 default:
                 // code block
             }
@@ -65,19 +90,32 @@ $(document).ready(function(){
                 case 'hamming':
                     dekodujKodowanieHamminga(daneZakodowane)
                 break;
-                case 'crc':
-                    dekodujCrc(daneZakodowane)
+                case 'crc12':
+                    var crc_12 = new Crc('12');
+                    crc_12.decodeCRC(daneZakodowane)
+                    break;
+                case 'crc16':
+                    var crc_16 = new Crc('16');
+                    crc_16.decodeCRC(daneZakodowane)
+                    break;
+                case 'crc32':
+                    var crc_32 = new Crc('32');
+                    crc_32.decodeCRC(daneZakodowane)
+                    break;
+                case 'crcSDLC':
+                    var crc_sdlc = new Crc('SDLC');
+                    crc_sdlc.decodeCRC(daneZakodowane)
                 break;
                 default:
                 // code block
             }
-       } 
+       }
    });
 
    $('#btnZak').click(function(){
      let ileZaklocen = $('#inputZak').val();
      let dane = $('#daneZakodowane1').text();
-    
+
      dane = stringToArray(dane);
      dane2 = stringToArray(dane);
      ile=0;
@@ -93,7 +131,7 @@ $(document).ready(function(){
             //console.log(ile)
         }
      }
-    
+
      $('#daneZakodowane').text(dane.join(''));
      $('#danePoKorekcji').text('');
      $('#daneWyjściowe').text('');
@@ -116,7 +154,7 @@ $(document).ready(function(){
             if(element != '0' && element != '1' ){
                 alert("Wprowadz 0 lub 1")
                 return false;
-            }     
+            }
         });
         if( !Number.isInteger(dane.length/8)){
             alert("Liczba powinna być podzielna przez 8")
@@ -128,12 +166,12 @@ $(document).ready(function(){
     /*Zamaina stringów na tablice int*/
     function stringToArray(string) {
         var dane = new Array(string.length);
-    
+
         for(var i=0; i<string.length; i++) {
             dane[i] = parseInt(string[i]);
         }
-        
+
         return dane;
     }
-    
+
 })
